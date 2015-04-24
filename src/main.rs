8d43944eadf54dda9aad57f8e87015a875d591a4
Wil Chung <iamwil@gmail.com>
2015-04-23 22:33:56 -0700

    can use math functions

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod english;
 use english::greeting;
 
 mod libfab;
+use libfab::tree::math;
 
 fn main() {
-    //let result = add_f(3.0, 4.0);
-    //println!("sum: {}", result); 
+    let result = math::add_f(3.0, 4.0);
+    println!("sum: {}", result); 
+
     println!("hello world");
     println!("Hello in english: {}", greeting::hello());
 }
