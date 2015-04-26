454d4718b3083aa4a95a75c85d8b32735396f029
Wil Chung <iamwil@gmail.com>
2015-04-26 15:38:41 -0700

    add all float math operators

--- src/libfab/tree/math.rs
+++ src/libfab/tree/math.rs
@@ -1,4 +1,4 @@
-use std::num::Float;
+use std::f32;
 
 // binary operators
 
@@ -32,18 +32,69 @@ pub fn pow_f(a: f32, b: f32) -> f32 {
 
 // unary operators
 
-//pub fn abs_f(a: f32) -> f32 {
-//}
+pub fn abs_f(a: f32) -> f32 {
+    a.abs()
+}
 
 pub fn square_f(a: f32) -> f32 {
     a * a
 }
 
-//pub fn sqrt_f(a: f32) -> f32 {
-//    return 
-//}
+pub fn sqrt_f(a: f32) -> f32 {
+    a.sqrt()
+}
+
+pub fn sin_f(a: f32) -> f32 {
+    a.sin()
+}
+
+pub fn cos_f(a: f32) -> f32 {
+    a.cos()
+}
+
+pub fn tan_f(a: f32) -> f32 {
+    a.tan()
+}
+
+pub fn asin_f(a: f32) -> f32 {
+    if a < -1.0 {
+        -f32::consts::PI / 2.0
+    } else if a > 1.0 {
+        f32::consts::PI / 2.0
+    } else {
+        a.asin()
+    }
+}
+
+pub fn acos_f(a: f32) -> f32 {
+    if a < -1.0 {
+        f32::consts::PI
+    } else if a > 1.0 {
+        0.0
+    } else {
+        a.acos()
+    }
+}
+
+pub fn atan_f(a: f32) -> f32 {
+    a.atan()
+}
 
 pub fn neg_f(a: f32) -> f32 {
     -a
+}
+
+//////////////// access
+
+pub fn X_f(x: f32) -> f32 {
+    x
+}
+
+pub fn Y_f(y: f32) -> f32 {
+    y
+}
+
+pub fn Z_f(z: f32) -> f32 {
+    z
 }
 
